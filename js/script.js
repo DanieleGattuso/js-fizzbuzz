2e5c06635e53bb010d4125de5bb25d93a85a5bb4
daniele.gattuso <d.gattuso09@gmail.com>
2024-12-18 21:26:46 +0100

    corrected an error

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 // stampiamo i valori di i da 0 a 100
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
 // Diamo un nome alla variabile nello scope chiuso
 
@@ -34,6 +34,7 @@ for (let i = 0; i <= 100; i++) {
 
     console.log(value);
 }
+
 
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
